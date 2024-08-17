4367134ac03e29e7e1604fa50fea145e75074128
ZZP-DMU <3571669089@qq.com>
2024-08-18 04:41:09 +0800

    add more func

--- src/extension.ts
+++ src/extension.ts
@@ -3,9 +3,44 @@ import { machine } from 'os';
 import { stringify } from 'querystring';
 import * as vscode from 'vscode';
 
-export function add_list(match_list_alias:any[], match_list_var:any[]) {
+export function getvar(b: vscode.TextLine, p: vscode.Position): string | any {
+	let tmps: string = "";
+	let bflag: boolean = false;
+	tmps = b.text.substring(p.character - 4, p.character + 4);
+	let regx = /%[^,;](\w+)[,;]/g;
+	let match = tmps.match(regx);
+	if (match) {
+		//console.log(match[0]);
+		return match[0].substring(1, match[0].length - 1);
+	}
+}
+
+export function isequal(a: string, b: vscode.TextLine, p: vscode.Position) {
+	let head: number = a.length;
+	let tail: number = 0;
+	for (let i = 0; i <= a.length; i++) {
+		if ((b.text.length > p.character + tail) && (p.character - head > 0)) {
+			//console.log(a,b.text.substring(p.character - head, p.character + tail),head,p.character,tail);
+			if (a === b.text.substring(p.character - head, p.character + tail)) {
+				//console.log(a,b.text.substring(p.character - head, p.character + tail));
+				return true;
+			}
+			else {
+				head--;
+				tail++;
+			}
+		}
+	}
+	return false;
+}
+
+export function add_list(match_list_alias: any[], match_list_var: any[]) {
 	console.log('here is add list');
-	//match_list_alias.empty();
+	while (match_list_alias.length > 0) {
+		match_list_alias.pop();
+		match_list_var.pop();
+	}
+	//match_list_alias =[];
 	//match_list_var = [];
 	const regex_a = /\/\/ ALIAS: (\w+) (\w+)/g;
 	const regex_1 = /\/\/ ALIAS: (\w+) (\w+)/;
@@ -18,16 +53,16 @@ export function add_list(match_list_alias:any[], match_list_var:any[]) {
 			//便利 lines
 			for (let i = 0; i < lines.length; i++) {
 				let line = lines[i];
-				if(line){
+				if (line) {
 					let match = line.match(regex_a);
 					console.log(`match: ${match}`);
-					if(match){
-						for(let j = 0; j < match.length; j++){
-								if(match[j] !==null){
+					if (match) {
+						for (let j = 0; j < match.length; j++) {
+							if (match[j] !== null) {
 								let tmp = match[j].match(regex_1);
-								if(tmp){
-								match_list_alias.push(tmp[2]);
-								match_list_var.push(tmp[1]);
+								if (tmp) {
+									match_list_alias.push(tmp[2]);
+									match_list_var.push(tmp[1]);
 								}
 							}
 						}
@@ -43,49 +78,55 @@ export function add_list(match_list_alias:any[], match_list_var:any[]) {
 		//how to pass match_list to disposable
 	}
 }
-	export function activate(context: vscode.ExtensionContext) {
-		console.log('Congratulations, your extension "my-first-extension" is now active!');
-		let match_list_alias: string[] = [];
-		let match_list_var: string[] = [];
-		add_list(match_list_alias, match_list_var);
-		//console.log(match_list_var);
-		//console.log(match_list_alias);
-		let disposable = vscode.languages.registerHoverProvider('c', {
-			provideHover(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken): vscode.ProviderResult<vscode.Hover> {
-				const line = document.lineAt(position);
-				const text = line.text;
-				//const regex = /\/\/\sALIAS:\s(.+)\s(.+)/;
-				// 假设注释格式为 // ALIAS: variableName aliasName 
-				//console.log(match_list_var);
-				//console.log(match_list_alias); 
-				//遍历match_list_var
-				let bflag:boolean = false;
-				for (let i = 0; i < match_list_var.length; i++) {
-						if (text.match(match_list_var[i])) {
-							//判断选中的变量是否是匹配项
-							//console.log(line.text.substring(position.character - match_list_var[i].length, position.character));
-							if (match_list_var[i] === line.text.substring(position.character - match_list_var[i].length, position.character)) {
-								bflag = true;
-								return new vscode.Hover(`${match_list_var[i]} Alias for: ${match_list_alias[i]}`);
-							}
-							else {
-								bflag = false;
-							}
-						}
+export function activate(context: vscode.ExtensionContext) {
+	console.log('Congratulations, your extension "my-first-extension" is now active!');
+	let match_list_alias: string[] = [];
+	let match_list_var: string[] = [];
+	add_list(match_list_alias, match_list_var);
+	//console.log(match_list_var);
+	//console.log(match_list_alias);
+	let disposable = vscode.languages.registerHoverProvider('c', {
+		provideHover(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken): vscode.ProviderResult<vscode.Hover> {
+			const line = document.lineAt(position);
+			const text = line.text;
+
+			//console.log(match_list_alias,match_list_var);
+			//const regex = /\/\/\sALIAS:\s(.+)\s(.+)/;
+			// 假设注释格式为 // ALIAS: variableName aliasName 
+			//console.log(match_list_var);
+			//console.log(match_list_alias); 
+			//遍历match_list_var
+			let bflag: number = 0;
+			for (let i = 0; i < match_list_var.length; i++) {
+				if (text.match(match_list_var[i])) {
+					//判断选中的变量是否是匹配项
+					//console.log(line.text.substring(position.character - match_list_var[i].length, position.character));
+					//if (match_list_var[i] === line.text.substring(position.character - match_list_var[i].length, position.character)) 
+					if (isequal(match_list_var[i], line, position)) {
+						bflag = 1;
+						return new vscode.Hover(`${match_list_var[i]} --> ${match_list_alias[i]}`);
+					}
+					else {
+						bflag = 2;
+					}
 				}
-				if(bflag === false){
-					return new vscode.Hover(`${line.text.substring(position.character - 1, position.character)} -> none`);
-				}
-				return null;
 			}
-		});
-		//console.log("i am here ?");
-		vscode.workspace.onDidChangeTextDocument(event => {  
-			// 当文档内容变化时，执行以下函数  
-			add_list(match_list_alias, match_list_var);  
-		});  
+			if (bflag === 2) {
+				let tmps = getvar(line,position);
+				return new vscode.Hover(`${tmps} --> not found`);
+			}
+			if (bflag === 0) {
+				return new vscode.Hover('none');
+			}
+		}
+	});
+	//console.log("i am here ?");
+	vscode.workspace.onDidChangeTextDocument(() => {
+		// 当文档内容变化时，执行以下函数  
+		add_list(match_list_alias, match_list_var);
+	});
 
-		context.subscriptions.push(disposable);
-	}
+	context.subscriptions.push(disposable);
+}
 
-	export function deactivate() { }
+export function deactivate() { }
